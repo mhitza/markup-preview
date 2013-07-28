3b3b4c1ba4c9d9ec74345a7e6505c2ccb0c09461
Marius Ghita <mhitza@gmail.com>
2013-07-28 06:29:46 +0300

    Toolbar and open button with file type filters

--- Main.hs
+++ Main.hs
@@ -7,19 +7,59 @@ module Main where
     import Control.Monad (void)
 
 
+    createToolbar = do
+        toolbar <- G.toolbarNew
+        G.toolbarSetStyle toolbar G.ToolbarIcons
+        openButton <- G.toolButtonNewFromStock "gtk-open"
+        void $ G.onToolButtonClicked openButton $ do
+            dialog <- G.fileChooserDialogNew 
+                        (Just "Choose a markup file")
+                        Nothing
+                        G.FileChooserActionOpen
+                        [("Ok", G.ResponseAccept), ("Cancel", G.ResponseCancel)]
+            textFileFilter <- G.fileFilterNew
+            G.fileFilterSetName textFileFilter "Text files"
+            G.fileFilterAddMimeType textFileFilter "text/plain"
+            G.fileChooserAddFilter dialog textFileFilter
+            markdownFilter <- G.fileFilterNew
+            G.fileFilterSetName markdownFilter "Markdown"
+            G.fileFilterAddPattern markdownFilter "*.md"
+            G.fileFilterAddPattern markdownFilter "*.markdown"
+            G.fileChooserAddFilter dialog markdownFilter
+            reStructuredTextFilter <- G.fileFilterNew
+            G.fileFilterSetName reStructuredTextFilter "reStructuredText"
+            G.fileFilterAddPattern reStructuredTextFilter "*.rst"
+            G.fileFilterAddPattern reStructuredTextFilter "*.rest"
+            G.fileFilterAddPattern reStructuredTextFilter "*.restx"
+            G.fileChooserAddFilter dialog reStructuredTextFilter
+            textileFilter <- G.fileFilterNew
+            G.fileFilterSetName textileFilter "Textile"
+            G.fileFilterAddPattern textileFilter "*.textile"
+            G.fileChooserAddFilter dialog textileFilter
+            G.widgetShow dialog
+
+        G.toolbarInsert toolbar openButton 0
+
+        return toolbar
+
+
     createInterface = do
         window <- G.windowNew
         scrolledWindow <- G.scrolledWindowNew Nothing Nothing
         webView <- GW.webViewNew
         G.set scrolledWindow [ G.containerChild := webView ]
+        statusBar <- G.statusbarNew
+        toolBar <- createToolbar
 
         singleColumn <- G.vBoxNew False 1
+        G.boxPackStart singleColumn toolBar G.PackNatural 0
         G.boxPackStart singleColumn scrolledWindow G.PackGrow 0
+        G.boxPackEnd singleColumn statusBar G.PackNatural 0
 
         G.set window [ G.containerChild := singleColumn
                      , G.windowDefaultWidth := 600
                      , G.windowDefaultHeight := 600
-                     , G.containerBorderWidth := 2
+                     , G.containerBorderWidth := 1
                      ]
         return (window, webView)
 
