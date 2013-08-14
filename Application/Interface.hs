f68da9ea8f8f2b5c01701f054d548fa6f6c8e7f6
Marius Ghita <mhitza@gmail.com>
2013-08-15 01:06:30 +0300

    Move interface creation into a separate module

---
+++ Application/Interface.hs
@@ -0,0 +1,97 @@
+module Application.Interface where
+
+
+    import Graphics.UI.Gtk (AttrOp((:=)))
+    import qualified Graphics.UI.Gtk as G
+    import qualified Graphics.UI.Gtk.WebKit.WebView as GW
+
+    import Paths_markup_preview
+
+    import Control.Monad (void, when)
+    import Control.Monad.Trans (lift)
+    import Control.Monad.Trans.Maybe
+    import Data.Maybe (isJust, fromJust)
+    import Control.Concurrent.MVar (putMVar)
+    import GHC.IO.Handle (hPutStr, hFlush)
+    import System.Directory (getTemporaryDirectory, getModificationTime)
+    import System.IO.Temp (openTempFile)
+    import Text.Pandoc (renderTemplate)
+
+
+    createFilter name filepatterns = do
+        fileFilter <- G.fileFilterNew
+        mapM_ (G.fileFilterAddPattern fileFilter) filepatterns
+        G.fileFilterSetName fileFilter name
+        return fileFilter
+
+
+    createOpenDialog = do
+        dialog <- G.fileChooserDialogNew 
+                    (Just "Choose a markup file")
+                    Nothing
+                    G.FileChooserActionOpen
+                    [("Ok", G.ResponseAccept), ("Cancel", G.ResponseCancel)]
+        createFilter "Markdown" ["*.md", "*.markdown"] >>= G.fileChooserAddFilter dialog
+        createFilter "reStructuredText" ["*.rst", "*.rest", "*.restx"] >>= G.fileChooserAddFilter dialog 
+        createFilter "Textile" ["*.textile"] >>= G.fileChooserAddFilter dialog 
+
+        return dialog
+
+
+    createToolbar loadNotifier = do
+        toolbar <- G.toolbarNew
+        G.toolbarSetStyle toolbar G.ToolbarIcons
+        openButton <- G.toolButtonNewFromStock "gtk-open"
+        void $ G.onToolButtonClicked openButton $ do
+            openDialog <- createOpenDialog
+            dialogResponse <- G.dialogRun openDialog
+            when (dialogResponse == G.ResponseAccept) $ do
+                response <- runMaybeT $ do
+                    filepath <- MaybeT $ G.fileChooserGetFilename openDialog
+                    fileFilter <- MaybeT $  G.fileChooserGetFilter openDialog
+                    format <- lift $ G.fileFilterGetName fileFilter 
+                    return (format, filepath)
+                when (isJust response) $ putMVar loadNotifier (fromJust response)
+            G.widgetDestroy openDialog
+
+        G.toolbarInsert toolbar openButton 0
+
+        return toolbar
+
+
+    createInterface loadNotifier = do
+        window <- G.windowNew
+        scrolledWindow <- G.scrolledWindowNew Nothing Nothing
+        webView <- GW.webViewNew
+        G.set scrolledWindow [ G.containerChild := webView ]
+        statusBar <- G.statusbarNew
+        toolBar <- createToolbar loadNotifier
+
+        singleColumn <- G.vBoxNew False 1
+        G.boxPackStart singleColumn toolBar G.PackNatural 0
+        G.boxPackStart singleColumn scrolledWindow G.PackGrow 0
+        G.boxPackEnd singleColumn statusBar G.PackNatural 0
+
+        G.set window [ G.containerChild := singleColumn
+                     , G.windowDefaultWidth := 600
+                     , G.windowDefaultHeight := 600
+                     , G.containerBorderWidth := 1
+                     ]
+        return (window, webView)
+
+
+    loadHtmlInView webView htmlContent = do
+            tempDirectory <- getTemporaryDirectory
+            layout <- getDataFileName "Resources/layout.html" >>= \filepath -> readFile filepath
+            let htmlContent' = renderTemplate [("htmlContent", htmlContent)] layout
+            (tempFilePath, tempHandle) <- openTempFile tempDirectory "markup-preview.html"
+            hPutStr tempHandle htmlContent' >> hFlush tempHandle
+            GW.webViewLoadUri webView ("file://" ++ tempFilePath)
+
+
+    withGUI f = do
+        void G.initGUI
+        window <- f
+        G.onDestroy window G.mainQuit
+        G.widgetShowAll window
+        G.mainGUI
--- Main.hs
+++ Main.hs
@@ -1,9 +1,6 @@
 module Main where
 
-    -- GUI related imports
-    import Graphics.UI.Gtk (AttrOp((:=)))
-    import qualified Graphics.UI.Gtk as G
-    import qualified Graphics.UI.Gtk.WebKit.WebView as GW
+    import Application.Interface
 
     -- logic related imports
     import Control.Monad (void, forever, when)
@@ -11,17 +8,10 @@ module Main where
     import Control.Concurrent (forkIO, threadDelay)
     import Control.Concurrent.MVar
     import Data.Maybe (isJust, fromJust)
-    import Control.Monad.Trans (lift)
-    import Control.Monad.Trans.Maybe
 
     -- transformation related imports
     import Text.Pandoc
     import System.Directory (getTemporaryDirectory, getModificationTime)
-    import System.IO.Temp (openTempFile)
-    import GHC.IO.Handle (hPutStr, hFlush)
-
-    -- misc
-    import Paths_markup_preview
 
     import Debug.Trace
 
@@ -30,85 +20,6 @@ module Main where
         intermediaryDocument = 
             let reader = fromJust $ lookup format [("Markdown", readMarkdown), ("reStructuredText", readRST), ("Textile", readTextile)]
             in reader (def { readerStandalone = True }) contents
-
-
-    createFilter name filepatterns = do
-        fileFilter <- G.fileFilterNew
-        mapM_ (G.fileFilterAddPattern fileFilter) filepatterns
-        G.fileFilterSetName fileFilter name
-        return fileFilter
-
-
-    createOpenDialog = do
-        dialog <- G.fileChooserDialogNew 
-                    (Just "Choose a markup file")
-                    Nothing
-                    G.FileChooserActionOpen
-                    [("Ok", G.ResponseAccept), ("Cancel", G.ResponseCancel)]
-        createFilter "Markdown" ["*.md", "*.markdown"] >>= G.fileChooserAddFilter dialog
-        createFilter "reStructuredText" ["*.rst", "*.rest", "*.restx"] >>= G.fileChooserAddFilter dialog 
-        createFilter "Textile" ["*.textile"] >>= G.fileChooserAddFilter dialog 
-
-        return dialog
-
-
-    createToolbar loadNotifier = do
-        toolbar <- G.toolbarNew
-        G.toolbarSetStyle toolbar G.ToolbarIcons
-        openButton <- G.toolButtonNewFromStock "gtk-open"
-        void $ G.onToolButtonClicked openButton $ do
-            openDialog <- createOpenDialog
-            dialogResponse <- G.dialogRun openDialog
-            when (dialogResponse == G.ResponseAccept) $ do
-                response <- runMaybeT $ do
-                    filepath <- MaybeT $ G.fileChooserGetFilename openDialog
-                    fileFilter <- MaybeT $  G.fileChooserGetFilter openDialog
-                    format <- lift $ G.fileFilterGetName fileFilter 
-                    return (format, filepath)
-                when (isJust response) $ putMVar loadNotifier (fromJust response)
-            G.widgetDestroy openDialog
-
-        G.toolbarInsert toolbar openButton 0
-
-        return toolbar
-
-
-    createInterface loadNotifier = do
-        window <- G.windowNew
-        scrolledWindow <- G.scrolledWindowNew Nothing Nothing
-        webView <- GW.webViewNew
-        G.set scrolledWindow [ G.containerChild := webView ]
-        statusBar <- G.statusbarNew
-        toolBar <- createToolbar loadNotifier
-
-        singleColumn <- G.vBoxNew False 1
-        G.boxPackStart singleColumn toolBar G.PackNatural 0
-        G.boxPackStart singleColumn scrolledWindow G.PackGrow 0
-        G.boxPackEnd singleColumn statusBar G.PackNatural 0
-
-        G.set window [ G.containerChild := singleColumn
-                     , G.windowDefaultWidth := 600
-                     , G.windowDefaultHeight := 600
-                     , G.containerBorderWidth := 1
-                     ]
-        return (window, webView)
-
-
-    withGUI f = do
-        void G.initGUI
-        window <- f
-        G.onDestroy window G.mainQuit
-        G.widgetShowAll window
-        G.mainGUI
-
-
-    loadHtmlInView webView htmlContent = do
-            tempDirectory <- getTemporaryDirectory
-            layout <- getDataFileName "Resources/layout.html" >>= \filepath -> readFile filepath
-            let htmlContent' = renderTemplate [("htmlContent", htmlContent)] layout
-            (tempFilePath, tempHandle) <- openTempFile tempDirectory "markup-preview.html"
-            hPutStr tempHandle htmlContent' >> hFlush tempHandle
-            GW.webViewLoadUri webView ("file://" ++ tempFilePath)
 
 
     loaderReloader webView loadNotifier Nothing Nothing = do
