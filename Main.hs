1cceef93249a09a28ad3837f4fa6c3a3d536fac2
Marius Ghita <mhitza@gmail.com>
2013-08-18 06:04:49 +0300

    Inline loaderReloader inside main

--- Main.hs
+++ Main.hs
@@ -4,40 +4,18 @@ module Main where
     import Application.CommandLine
     import Application.FileHandling
 
-    import Graphics.UI.Gtk.WebKit.WebView (WebViewClass)
-    import Data.Time.Clock (UTCTime(..))
-
     -- logic related imports
     import Control.Monad (void, when)
     import Control.Concurrent (forkIO, threadDelay)
     import Control.Concurrent.MVar
+    import Data.Maybe (isNothing, fromJust)
 
     -- transformation related imports
     import System.Directory (getModificationTime)
 
 
-    loaderReloader :: WebViewClass self
-                   => self
-                   -> MVar (String, FilePath)
-                   -> Maybe (String, FilePath)
-                   -> Maybe UTCTime
-                   -> IO b
-    loaderReloader webView loadNotifier Nothing Nothing = do
-        threadDelay 500
-        (format, filepath) <- takeMVar loadNotifier
-        modificationTime <- getModificationTime filepath
-        renderHtml (format, filepath) >>= loadFile webView
-        loaderReloader webView loadNotifier (Just (format, filepath)) (Just modificationTime)
-
-    loaderReloader webView loadNotifier (Just (format, filepath)) (Just modificationTime) = do
-        threadDelay 500
-        noNewFile <- isEmptyMVar loadNotifier
-        if noNewFile
-            then do modificationTime' <- getModificationTime filepath
-                    when (modificationTime' /= modificationTime) $ do
-                        renderHtml (format, filepath) >>= loadFile webView
-                    loaderReloader webView loadNotifier (Just (format, filepath)) (Just modificationTime')
-            else loaderReloader webView loadNotifier Nothing Nothing
+    loop :: Monad m => a -> (a -> m a) -> m a
+    loop a f = f a >>= \a' -> loop a' f
 
 
     main :: IO ()
@@ -46,5 +24,17 @@ module Main where
             loadNotifier <- case initialLoad of Just x  -> newMVar x
                                                 Nothing -> newEmptyMVar
             (window, webView) <- createInterface loadNotifier
-            void $ forkIO $ loaderReloader webView loadNotifier Nothing Nothing
+            void . forkIO . void $ loop (Nothing, Nothing) $ \(resource, modificationTime) -> do
+                threadDelay 500
+                if isNothing resource || isNothing modificationTime
+                    then do resource' <- takeMVar loadNotifier
+                            modificationTime' <- getModificationTime (snd resource')
+                            renderHtml resource' >>= loadFile webView
+                            return (Just resource', Just modificationTime')
+                    else do noNewFile <- isEmptyMVar loadNotifier
+                            if noNewFile
+                                then do modificationTime' <- getModificationTime (snd $ fromJust resource)
+                                        when (modificationTime' /= (fromJust modificationTime)) $ renderHtml (fromJust resource) >>= loadFile webView
+                                        return (resource, Just modificationTime')
+                                else return (Nothing, Nothing)
             return window
