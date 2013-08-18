00e48db747e483e8ec8fec5380395b673a62e1dd
Marius Ghita <mhitza@gmail.com>
2013-08-18 03:10:30 +0300

    Add option to load file from the command line

--- Main.hs
+++ Main.hs
@@ -4,12 +4,16 @@ module Main where
     import Graphics.UI.Gtk.WebKit.WebView (WebViewClass)
     import Data.Time.Clock (UTCTime(..))
 
+    -- command line interface imports
+    import System.Console.CmdArgs.Explicit
+
     -- logic related imports
     import Control.Monad (void, when)
-    import System.Posix.Process (forkProcess)
+    import Control.Applicative ((<$>))
     import Control.Concurrent (forkIO, threadDelay)
     import Control.Concurrent.MVar
     import Data.Maybe (fromJust)
+    import Data.List (isSuffixOf, find)
 
     -- transformation related imports
     import Text.Pandoc
@@ -49,9 +53,31 @@ module Main where
             else loaderReloader webView loadNotifier Nothing Nothing
 
 
+    arguments :: Mode [(String,String)]
+    arguments = mode "markup-preview" [] "" (flagArg (upd "file") "file")
+        [ flagHelpSimple (("help",""):)
+        , flagVersion (("version",""):)
+        ]
+        where upd msg x v = Right $ (msg,x):v
+
+
+    getArgumentFile :: FilePath -> Maybe (String, FilePath)
+    getArgumentFile filepath = flip (,) filepath . fst <$> find (any (`isSuffixOf` filepath) . snd)
+                                    [ ("Markdown", [".markdown", ".md"])
+                                    , ("Textile", [".textile"])
+                                    , ("reStructuredText", [".rst", ".rest", ".restx"]) ]
+
+
     main :: IO ()
-    main = void $ forkProcess $ withGUI $ do
-        loadNotifier <- newEmptyMVar :: IO (MVar (String, String))
-        (window, webView) <- createInterface loadNotifier
-        void $ forkIO $ loaderReloader webView loadNotifier Nothing Nothing
-        return window
+    main = do
+        args <- processArgs arguments
+        let hasFlag flag = (flag, "") `elem` args
+        let initialLoad = lookup "file" args >>= getArgumentFile
+        if hasFlag "help"
+            then print $ helpText [] HelpFormatDefault arguments
+            else withGUI $ do
+                loadNotifier <- case initialLoad of Just x  -> newMVar x
+                                                    Nothing -> newEmptyMVar
+                (window, webView) <- createInterface loadNotifier
+                void $ forkIO $ loaderReloader webView loadNotifier Nothing Nothing
+                return window
