9696ad634992cd4135c214d0c9017b9e34cf37bb
Marius Ghita <mhitza@gmail.com>
2013-09-04 04:29:29 +0300

    Add system dependencies into a puppet manifest

---
+++ external.pp
@@ -0,0 +1,15 @@
+package { 'cairo-devel':
+  ensure => 'present',
+}
+
+package { 'pango-devel':
+  ensure => 'present',
+}
+
+package { 'gtk2-devel':
+  ensure => 'present',
+}
+
+package { 'webkitgtk-devel':
+  ensure => 'present',
+}
